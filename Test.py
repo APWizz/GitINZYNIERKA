cc313dea35f716b96ed4d7f1a0e8ec2c2f2667aa
Adrain Porczak <adrian.porczak14@gmail.com>
2024-04-10 21:20:11 +0200

    10.04.2024 utworzenie pliku testowego w jednym kodzie do konwersji i nastepnie wyswietlenia

--- Test.py
+++ Test.py
@@ -1,33 +1,29 @@
 import vtk
+import meshio
 
-# Ścieżka do pliku OBJ
-obj_file_path = r"C:\Users\adria\Desktop\Git 1\output.obj"
+def convert_stl_to_obj(input_stl_file, output_obj_file):                # definiuje funkcje do konwersji formatu pliku
+    mesh = meshio.read(input_stl_file)                                  # Wczytaj plik STL
+    meshio.write(output_obj_file, mesh, file_format='obj')              # Zapisz siatkę do formatu OBJ
 
-# Wczytaj plik OBJ
-reader = vtk.vtkOBJReader()
-reader.SetFileName(obj_file_path)
-reader.Update()
 
-# Utwórz aktora na podstawie wczytanych danych
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputData(reader.GetOutput())
+def render_obj_file(output_obj_file):                                     # definiuje funkcje do wyświetlania bryly
+    reader = vtk.vtkOBJReader()                                         # Wczytaj plik OBJ
+    reader.SetFileName(output_obj_file)
+    reader.Update()
+    mapper = vtk.vtkPolyDataMapper()                                    # Utwórz aktora na podstawie wczytanych danych
+    mapper.SetInputData(reader.GetOutput())
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    renderer = vtk.vtkRenderer()                                        # Utwórz renderer i dodaj aktora do niego
+    renderer.AddActor(actor)
+    render_window = vtk.vtkRenderWindow()                               # Utwórz okno renderowania
+    render_window.AddRenderer(renderer)
+    render_window_interactor = vtk.vtkRenderWindowInteractor()          # Utwórz interakcję
+    render_window_interactor.SetRenderWindow(render_window)
+    render_window.Render()                                              # Uruchom interakcję
+    render_window_interactor.Start()
 
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-
-# Utwórz renderer i dodaj aktora do niego
-renderer = vtk.vtkRenderer()
-renderer.AddActor(actor)
-
-# Utwórz okno renderowania
-render_window = vtk.vtkRenderWindow()
-render_window.AddRenderer(renderer)
-
-# Utwórz interakcję
-render_window_interactor = vtk.vtkRenderWindowInteractor()
-render_window_interactor.SetRenderWindow(render_window)
-
-# Uruchom interakcję
-render_window.Render()
-render_window_interactor.Start()
-
+input_stl_file = r"C:\Users\adria\Desktop\Inzynierka\Pręt_test.stl"     # Ścieżka do pliku STL wejściowego
+output_obj_file = 'output.obj'                                          # Ścieżka do pliku OBJ wyjściowego
+convert_stl_to_obj(input_stl_file, output_obj_file)                     # Wywołaj funkcję konwersji
+render_obj_file(output_obj_file)                                          # Wywołanie funkcji
